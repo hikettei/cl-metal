26dc03018e8e535147bea7b45e532f3e6918c6f0
hikettei <hikettei@gmail.com>
2023-12-18 14:06:37 +0900

    Enhancement: lambda, iterators to the metalize

--- source/metalize.lisp
+++ source/metalize.lisp
@@ -22,6 +22,68 @@
 			     (read-from-string
 			      (format nil "~a" form)))))
     (trivia:ematch form
+
+      ;; Iteration over var
+      ;; (dotimes (i var) ..)
+      ((list* 'dotimes (list bind count) _)
+       (format nil "for (auto ~a=0; ~a<~a; ~a+=1) {~%~a~%}"
+	       bind bind count bind
+	       (metalize-form `(progn ,@(cddr form)))))
+
+      ;; Iteration over single variable
+      ;; (for (x 0) (< x 1) (incf x)
+      ;; ...)
+      ((list* 'for
+	      (list (type symbol) initial)
+	      (list* cond)
+	      (list* steps)
+	      _)
+       (format nil "for (auto ~a=~a;~a;~a) {~%~a~%}"
+	       (metalize-form (car (second form)))
+	       (metalize-form initial)
+	       (metalize-form cond)
+	       (metalize-form steps)
+	       (metalize-form `(progn ,@(cddddr form)))))
+      
+      ;; Iteration (rich)
+      ;; (for ((x 0)     (y 0) ...)
+      ;;      ((x < 1)   (y < 1) ...)
+      ;;      ((x += 1)  (y += 1) ...)
+      ;;  body)
+
+      ((list* 'for
+	      (list* decls)
+	      (list* conds)
+	      (list* steps)
+	      _)
+       (format nil "for (~a;~a;~a);{~%~a~%}"
+	       (map-split ", "
+			  #'(lambda (decl)
+			      (format nil "auto ~a=~a"
+				      (metalize-form (car decl))
+				      (metalize-form (second decl))))
+			  decls)
+	       (map-split ", " #'metalize-form conds)
+	       (map-split ", " #'metalize-form steps)
+	       (metalize-form `(progn ,@(cddddr form)))))
+
+      ;; lambda
+      ;; (lambda ((int x) (int y)) (+ x y))
+      ;; I found that lambda expression is not supported in Metal(C++14)
+      ;; so the code below is useless
+      ((list* 'lambda (list* args) (list* forms))
+       (format nil "[](~a){ return ~a; };"
+	       (map-split
+		", "
+		#'(lambda (arg)
+		    (assert (= (length arg) 2)
+			    ()
+			    "Metalize-form: Cannot metalize the form ~% ~a ~%because `~a` must be (dtype bind)"
+			    form arg)
+		    (format nil "~a ~a" (metalize-form (first arg)) (metalize-form (second arg))))
+		args)
+	       (metalize-form `(progn ,@forms))))
+      
       ;; (- a) -> -a
       ((list* '- form)
        (format nil "-~a" (metalize-form (car form))))
@@ -41,12 +103,13 @@
 	       (metalize-form a)
 	       (metalize-form b)))
       ;; A = B, A+=B, A-=B, A*=B, A/=B
-      ((list (or 'incf 'decf 'setf 'mulcf 'divcf) form1 form2)
-       (format nil
-	       "~a ~a ~a"
-	       (metalize-form form1)
-	       (cSymbol (car form))
-	       (metalize-form form2)))
+      ((list (or 'incf 'decf 'setf 'mulcf 'divcf) form1)
+       (let ((form2 (or (third form) 1)))
+	 (format nil
+		 "~a ~a ~a"
+		 (metalize-form form1)
+		 (cSymbol (car form))
+		 (metalize-form form2))))
       ;; (if exp then &optional else)
       ((list* 'if exp _)
        (flet ((helper (form)
@@ -102,7 +165,10 @@
 ;;  - prepare compatible specifications with cl-cuda
 
 (defmacro with-metalize (&rest forms)
-  "[Experimental] Translates the given S-expressions into a metal expression"
+  "[Experimental] Translates the given S-expressions into a metal expression.
+
+
+"
   (let ((metalized-form
 	  (cl-ppcre:regex-replace-all
 	   "};"
